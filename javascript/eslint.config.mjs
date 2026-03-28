4e64a4222d6f5a8bceaea7edadaab78c72996c99
chris <chris@buildlackey.com>
2026-03-28 15:04:04 -0700

    fix: commit missing eslint.config.mjs

---
+++ javascript/eslint.config.mjs
@@ -0,0 +1,6 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import baseConfig from "@datalackey/typescript-build-config/eslint";
+
+export default [...baseConfig, {}];
+
